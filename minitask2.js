d24345cc19c620d522970d16f881e563391a268b
abdullah-fikri <abdullahfikri981@gmail.com>
2025-09-03 17:20:26 +0700

    Add files via upload

--- minitask2.js
+++ minitask2.js
@@ -1,9 +1,34 @@
-
-for (let i =0; i<7; i++){
-    var a = ''
+for (let i =0; i<6; i++){
+    var a = ''    
     for(let x = 0; x<i; x++){
         a += '*'
         
     }
     console.log(a)
 }
+
+
+for (let i =6; i>0; i--){
+    let a = ''
+    for (let x=0; x<i; x++){
+        a += '*'
+    }
+    console.log(a)
+}
+
+
+
+var i = 0
+var a = ''
+do {
+    
+    let x = 0
+    do{
+        
+        a += '*'
+        x++;
+        console.log(a)
+    }while(x < i)
+        
+    i++;
+  } while (i < 4);
